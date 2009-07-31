7d8db5637f822375a368aae117a950a6021fa07c
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-07-31 19:55:48 -0400

    Added basic schema: users table.

---
+++ sql/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users (
+    id                 SERIAL          PRIMARY KEY,
+    username           VARCHAR( 64 )   UNIQUE,
+    encrypted_password VARCHAR( 512 ),
+    time_created       TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    openid             VARCHAR( 1024 ) UNIQUE,
+    admin              BOOLEAN         NOT NULL DEFAULT FALSE,
+    CONSTRAINT identifiable CHECK (
+        (
+            username IS NOT NULL
+            AND encrypted_password IS NOT NULL
+        ) OR openid IS NOT NULL
+    )
+);
+
