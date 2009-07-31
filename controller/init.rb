8ed2481c3d0cf2bb8cad71e37aa07c9ef37ff7b2
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-07-31 19:44:20 -0400

    Add json provide.  Put Controller under an application module.

--- controller/init.rb
+++ controller/init.rb
@@ -1,11 +1,14 @@
 # Define a subclass of Ramaze::Controller holding your defaults for all
 # controllers
 
-class Controller < Ramaze::Controller
-  layout :default
-  helper :xhtml
-  engine :Etanni
+module ApplicationName
+  class Controller < Ramaze::Controller
+    layout :default
+    helper :xhtml
+    engine :Etanni
+    provide( :json, :type => 'application/json' ) { |action, value| value.to_json }
+  end
 end
 
 # Here go your requires for subclasses of Controller:
-require __DIR__('main')
+require __DIR__( 'main' )
