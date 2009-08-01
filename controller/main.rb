095bac696fa63c3635835a86e643ce1d1ea8a2e8
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-07-31 20:17:48 -0400

    Added register, login, logout.

--- controller/main.rb
+++ controller/main.rb
@@ -14,6 +14,48 @@ module ApplicationName; module Controllers
 
     helper :stack, :user
 
+    def register
+      redirect_referrer  if logged_in?
+      return  if ! request.post?
+
+      username = request[ 'username' ]
+      password = request[ 'password' ]
+      u = Models::User[ username: username ]
+      if u
+        flash[ :error ] = "#{username} is already registered."
+      else
+        Models::User.register( username, password )
+      end
+      user_login( username: username, password: password )
+      if logged_in?
+        flash[ :success ] = "Registered #{username}."
+        answer rs( :/ )
+      else
+        flash[ :error ] = "Failed to register #{username}."
+      end
+    end
+
+    def login
+      redirect_referrer  if logged_in?
+
+      if request.post?
+        user_login(
+          :username => request[ 'username' ],
+          :password => request[ 'password' ]
+        )
+        if logged_in?
+          answer rs( :/ )
+        end
+      end
+    end
+
+    def logout
+      user_logout
+      redirect rs( :/ )
+    end
+
+    # -----------------------
+
     def index
       @title = "Home"
       if ! logged_in?
