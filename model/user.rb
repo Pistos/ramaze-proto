b14a40093cb407c97a723b6ad5942a33de45ed74
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-07-31 20:06:37 -0400

    Fill out user model, re: authentication.

--- model/user.rb
+++ model/user.rb
@@ -1,4 +1,36 @@
 module ApplicationName; module Models
+
   class User < DBI::Model( :users )
+
+    def self.authenticate( credentials )
+      return nil  if credentials.nil? || credentials.empty?
+
+      if credentials[ :api_key ]
+        self[ :api_key => credentials[ :api_key ] ]
+      elsif credentials[ :openid ]
+        self[ :openid => credentials[ :openid ] ]
+      elsif credentials[ :password ]
+        encrypted_password = Digest::SHA1.hexdigest( credentials[ :password ] )
+        self[
+          :username => credentials[ :username ],
+          :encrypted_password => encrypted_password
+        ]
+      end
+    end
+
+    def self.register( username, password )
+      encrypted_password = Digest::SHA1.hexdigest( password )
+      self.create( username: username, encrypted_password: encrypted_password )
+    end
+
+    def to_s
+      username || openid
+    end
+
+    def admin?
+      admin
+    end
+
   end
+
 end; end
